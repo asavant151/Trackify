2716b4489b44ef2c1b913aaed918a274d4fbdc1a
asavant151 <asavant151@gmail.com>
2026-03-04 15:53:04 +0530

    cors in changing

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:3000"
+    ],
+    credentials: true,
+}));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
