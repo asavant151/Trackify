fd331c8609c9264c121a90a04c5580a15501b27f
asavant151 <asavant151@gmail.com>
2026-03-04 16:03:07 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:5000"
+        "http://localhost:5000",
+        "https://trackify-jet.vercel.app"
     ],
     credentials: true,
 }));
